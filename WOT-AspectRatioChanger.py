af129e8f3b6dc8b0e1b3d3bdde154afddd71e485
VThang <leejaewoon9306@gmail.com>
2023-10-28 17:31:10 +0700

    0.0.1

--- WOT-AspectRatioChanger.py
+++ WOT-AspectRatioChanger.py
@@ -1,9 +1,7 @@
-from io import TextIOWrapper
 import os
 import sys
 import time
-
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 
 global list_aspect
 dict_aspect = {
@@ -17,74 +15,111 @@ dict_aspect = {
 
 def main():
     """
-    Execute the main logic of the program.
-    This function loads data from a file using the `load_data` function.
-    It then prompts the user to select a key using the `menu` function.
-    Finally, it calls the `change_aspect_ratio` function with the loaded data and the selected key.
-
-    Parameters:
-        None
-
-    Returns:
-        None
+    This function is the main entry point of the program.
+    It loads data using the `load_data` function,
+    prompts the user to select a menu option using the `menu` function,
+    gets the root element of the loaded data using the `getroot` method,
+    changes the aspect ratio of the root element using the `change_aspect_ratio` function,
+    saves the modified data using the `save_file` function.
     """
-    file = load_data()
+    element_tree = load_data()
     key = menu()
-
-    change_aspect_ratio(file, key)
+    root_element = element_tree.getroot()
+    change_aspect_ratio(root_element, key)
+    save_file(element_tree)
     print("VThang - 2023")
     time.sleep(4)
 
 
-def load_data() -> TextIOWrapper:
+def load_data() -> ET.ElementTree:
     """
-    Loads data from the preferences.xml file located in the %APPDATA%/Wargaming.net/WorldOfTanks directory.
+    Load data from the preferences.xml file located in the APPDATA directory.
 
     Returns:
-        TextIOWrapper: The file object representing the opened preferences.xml file.
+        ET.ElementTree: The parsed XML tree object representing the preferences.xml file.
+
+    Raises:
+        FileNotFoundError: If the preferences.xml file is not found.
+        ET.ParseError: If the preferences.xml file is malformed.
     """
     try:
         appdata = os.getenv("APPDATA")
         if appdata is None:
             print("APPDATA environment variable not found.\nProgram exited.")
+        global file_location
         file_location = appdata + "\\Wargaming.net\\WorldOfTanks\\preferences.xml"
         print(f"Loading: {file_location}")
-        file = open(file_location)
+        # file = open(file_location)
+        tree = ET.parse(file_location)
+        root = tree.getroot()
+        check = False
+        for x in root:
+            if x.tag == "devicePreferences":
+                for y in x:
+                    if y.tag == "aspectRatio":
+                        check = True
+                        print(f"Current settings is: {y.text}")
+        if check is False:
+            raise ET.ParseError
     except FileNotFoundError:
         print(
-            "Preference.xml file not found.\nTrying to run game first to create preferences.xml file then run this program again.\nProgram exited."
+            "Preference.xml file not found. Trying to run game first to create preferences.xml file then run this program again.\nProgram exited."
         )
         sys.exit(0)
-    return file
+    except ET.ParseError:
+        print(
+            "Preference.xml file malformed. Trying to run game first to recreate preferences.xml file then run this program again.\nProgram exited."
+        )
+        sys.exit(0)
+    return tree
 
 
-def change_aspect_ratio(file, key: str):
+def change_aspect_ratio(root_element: ET.Element, key: str) -> None:
     """
-    Changes the aspect ratio of the game.
+    Change the aspect ratio of the game window.
 
     Args:
-        file (file-like object): The XML file containing the game preferences.
+        root_element (ET.Element): The root element of the XML tree.
         key (str): The key representing the desired aspect ratio.
 
     Returns:
-        None
-
-    Prints the original and updated aspect ratios to the console.
+        None: This function does not return anything.
     """
     print(f"Game aspect ratio will be changed to: {key} ={dict_aspect[key]}")
-    pref_file = BeautifulSoup(file.read(), "xml")
-    before = pref_file.root.devicePreferences.aspectRatio.text
-    pref_file.root.devicePreferences.aspectRatio.string = dict_aspect[key]
-    after = pref_file.root.devicePreferences.aspectRatio.text
-    print(f"{before} -> {after}")
+    for x in root_element:
+        if x.tag == "devicePreferences":
+            for y in x:
+                if y.tag == "windowMode":
+                    y.text = "1"
+                if y.tag == "aspectRatio":
+                    y.text = dict_aspect[key]
+                if y.tag == "aspectRatio_override":
+                    y.text = dict_aspect[key]
+
+
+def save_file(element_tree: ET.ElementTree) -> None:
+    """
+    Save the given ElementTree to the specified file location.
+
+    Args:
+        element_tree (ET.ElementTree): The ElementTree object to be saved.
+
+    Returns:
+        None
+    """
+    element_tree.write(file_location)
 
 
 def menu() -> str:
     """
-    Prints a menu of screen aspect ratios and prompts the user to select one.
+    This function displays a menu for selecting a screen aspect ratio.
+    It iterates through the `dict_aspect` dictionary and prints the options.
+    The user is prompted to enter a number corresponding to their selection.
+    If the user enters an invalid number, a `ValueError` is raised.
+    If the user enters a valid number, the function returns the corresponding aspect ratio key.
 
-    Returns:
-        str: The selected screen aspect ratio.
+    :return: A string representing the selected aspect ratio key.
+    :rtype: str
     """
     print(
         "World of Tanks change aspect ratio mini tools."
